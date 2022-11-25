7f71250b3fa90ce99a6032b3f6f3553f2b1b2fa0
krist <krist@LAPTOP-HIQLHLL6>
2022-11-25 17:18:03 +0500

    Добавьте файлы проекта.

---
+++ Lab 14/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_14
+{
+     abstract class Animal
+    {
+        public abstract string Name { get; set; }
+
+        public Animal(string name)
+        {
+            Name = name;
+        }
+
+        public abstract void ToSay();
+
+
+    public void ShowInfo()
+        {
+            Console.WriteLine(Name);
+            ToSay();
+        }   
+    }
+
+    class Cat:Animal
+    {
+        private string name;
+        string say;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public Cat(string name, string say):base (name)
+        {
+            this.say = say;
+        }
+        public override void ToSay()
+        {
+            Console.WriteLine(say);
+        }
+    }
+    class Dog : Animal
+    {
+        private string name;
+        string say;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public Dog(string name, string say) : base(name)
+        {
+            this.say = say;
+        }
+        public override void ToSay()
+        {
+            Console.WriteLine(say);
+        }
+    }
+}
---
+++ Lab 14/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Cat cat = new Cat("кот", "мяу");
+            Dog dog = new Dog("пёс", "гав");
+            cat.ShowInfo( );
+            dog.ShowInfo();
+
+        }
+    }
+}
